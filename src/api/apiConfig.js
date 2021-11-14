e5da22ac567688663f097a92a5e56869e1f881be
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-11-14 14:13:04 +0100

    Updates

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '858562499d43d781fa0db3b6a90062d9',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
-}
-
-export default apiConfig;
